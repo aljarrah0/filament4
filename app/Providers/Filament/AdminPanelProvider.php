af3af6a81f436a168c04b109d874e2076319b6db
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-11-29 22:18:05 +0200

    Add registration method to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->sidebarCollapsibleOnDesktop()
             ->sidebarWidth('15rem')
             ->colors([
