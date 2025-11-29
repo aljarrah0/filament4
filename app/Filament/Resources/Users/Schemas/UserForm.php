fd221043f0f201e22c1bf536e9701ea5a9af88e7
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-11-29 22:51:28 +0200

    Add TextInput components for name, email, and password in UserForm

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -10,7 +11,9 @@ class UserForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')->label('Name')->required(),
+                TextInput::make('email')->label('Email')->email()->required(),
+                TextInput::make('password')->label('Password')->password()->required(),
             ]);
     }
 }
