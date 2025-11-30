5900c4d1d448c141edc5165dcbb1d9ba19ee61b2
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-11-30 02:33:54 +0200

    Update ProductStatusEnum to use human-readable status labels

--- app/Enum/ProductStatusEnum.php
+++ app/Enum/ProductStatusEnum.php
@@ -4,7 +4,7 @@ namespace App\Enum;
 
 enum ProductStatusEnum: string
 {
-    case IN_STOCK = 'in_stock';
-    case SOLD_OUT = 'sold_out';
-    case COMING_SOON = 'coming_soon';
+    case IN_STOCK = 'In Stock';
+    case SOLD_OUT = 'Sold Out';
+    case COMING_SOON = 'Coming Soon';
 }
