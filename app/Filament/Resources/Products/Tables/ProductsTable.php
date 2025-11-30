11519f936d216f566a89f89c0dffb3ccd90759c4
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-11-30 20:54:46 +0200

    Add created_at filter to ProductsTable for improved product date range filtering

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -5,11 +5,14 @@ namespace App\Filament\Resources\Products\Tables;
 use Filament\Tables\Table;
 use App\Enum\ProductStatusEnum;
 use Filament\Actions\EditAction;
+use Filament\Tables\Filters\Filter;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Actions\ForceDeleteBulkAction;
 
@@ -36,6 +39,18 @@ class ProductsTable
                 SelectFilter::make('category')
                     ->label('Category')
                     ->relationship('category', 'name'),
+                Filter::make('created_at')
+                    ->schema([
+                        DatePicker::make('created_from'),
+                        DatePicker::make('created_until')
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when($data['created_from'], function (Builder $query, $created_from): Builder {
+                            return $query->where('created_at', '>=', $created_from);
+                        })->when($data['created_until'], function (Builder $query, $created_until): Builder {
+                            return $query->where('created_at', '<=', $created_until);
+                        });
+                    }),
             ])
             ->recordActions([
                 EditAction::make(),
