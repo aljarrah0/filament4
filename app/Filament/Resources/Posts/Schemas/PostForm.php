b44c6c0eccd72f6b58b3d1763009a6deccced6a8
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-12-18 00:24:14 +0200

    Refactor PostForm to organize fields into collapsible sections for improved usability; separate content and meta information, enhancing the form layout and user experience.

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources\Posts\Schemas;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
 
@@ -17,36 +19,47 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->required(),
-                TextInput::make('slug')
-                    ->required(),
-                Select::make('user_id')
-                    ->relationship('user', 'name')
-                    ->required(),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->required(),
-                FileUpload::make('thumbnail')
-                    ->image()
-                    ->disk('public')
-                    ->directory('thumbnails')
-                    ->default(null),
-                TagsInput::make('tags')
-                    ->default(null)
-                    ->suggestions([
-                        'tailwindcss',
-                        'alpinejs',
-                        'laravel',
-                        'livewire',
-                    ])
-                    ->splitKeys(['Tab', ' '])
-                    ->color('danger'),
-                RichEditor::make('content')
-                    ->required()
-                    ->columnSpanFull(),
-                Toggle::make('is_published')
-                    ->required(),
-            ]);
+                Section::make('Content')
+                    ->description('The main content of the post.')
+                    ->collapsible()
+                    ->schema([
+                        TextInput::make('title')
+                            ->required(),
+                        TextInput::make('slug')
+                            ->required(),
+                        RichEditor::make('content')
+                        ->columnSpanFull(),
+                    ])->columnSpan(2),
+                Section::make('Meta')
+                    ->description('Meta information about the post.')
+                    ->collapsible()
+                    ->schema([
+                        Group::make()->schema([
+                            Select::make('user_id')
+                                ->relationship('user', 'name')
+                                ->required(),
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->required(),
+                            FileUpload::make('thumbnail')
+                                ->image()
+                                ->disk('public')
+                                ->directory('thumbnails')
+                                ->default(null),
+                            TagsInput::make('tags')
+                                ->default(null)
+                                ->suggestions([
+                                    'tailwindcss',
+                                    'alpinejs',
+                                    'laravel',
+                                    'livewire',
+                                ])
+                                ->splitKeys(['Tab', ' '])
+                                ->color('danger'),
+                            Toggle::make('is_published')
+                                ->required(),
+                        ]),
+                    ])->columnSpan(1),
+            ])->columns(3);
     }
 }
